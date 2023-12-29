fb58672ae130b313ac5e8a83e3f82057c31ee59a
yoonsun_jun <yoonsun_jun@tmax.co.kr>
2023-12-29 10:26:57 +0900

    [feat] 우선순위 배지 구현

---
+++ whats-in-my-todo/src/components/Priority.tsx
@@ -0,0 +1,41 @@
+import { Typography } from "@mui/material";
+
+interface Props {
+  text: string;
+}
+
+export const Priority = ({ text }: Props) => {
+  // 우선순위에 따라 글자 및 배경 색 바꾸기
+  let backgroundColor, fontColor;
+
+  switch (text) {
+    case "HIGH":
+      backgroundColor = "#FFD1D1";
+      fontColor = "#B50000";
+      break;
+    case "MEDIUM":
+      backgroundColor = "#FFED92";
+      fontColor = "#B65700";
+      break;
+    case "LOW":
+      backgroundColor = "#BEFF8B";
+      fontColor = "#00750C";
+      break;
+  }
+
+  return (
+    <Typography
+      sx={{
+        borderRadius: "4px",
+        fontSize: "10px",
+        fontWeight: 600,
+        backgroundColor: backgroundColor,
+        color: fontColor,
+        padding: "2px 4px",
+        marginRight: "16px",
+      }}
+    >
+      {text}
+    </Typography>
+  );
+};
